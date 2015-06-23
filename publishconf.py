85f8cab43e01998ac1f6c41d772937e33fb12f47
Tracy Poff <tracy.poff@gmail.com>
2015-06-23 19:31:07 -0400

    Use HTTPS.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://sopoforic.github.io/too-much-free-time'
+SITEURL = 'https://sopoforic.github.io/too-much-free-time'
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
